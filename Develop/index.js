558f2ea3b4c952e5c1c553ad4313ab10c56f6068
toastermaster27 <118187528+toastermaster27@users.noreply.github.com>
2023-02-12 21:15:42 -0500

    Committed changes to github

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,152 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const { title } = require('process');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your projects title? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Make sure to enter a title.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please describe what your project does. (Required)',
+        validate: descInput => {
+            if (descInput) {
+                return true;
+            } else {
+                console.log('Please provide a description.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'why',
+        message: 'Why did you create this project (Required)',
+        validate: whyInput => {
+            if (whyInput) {
+                return true;
+            } else {
+                console.log('Please explain why you created this project.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide instructions for installation of your project. (Required)',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('Please provide instructions on how to install your project.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How can this project be used?',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please explain how your project can be used.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'learn',
+        message: 'What did you learn while creating this project?',
+        validate: learnInput => {
+            if (learnInput) {
+                return true;
+            } else {
+                console.log('Please enter wht you learned during this project.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'licensing',
+        message: 'Which license will you use for your project?',
+        choices: ['agpl', 'lgpl', 'osl-3.0', 'No license']
+    },
+    {
+        type: 'confirm',
+        name: 'contributers confirmation',
+        message: 'Are other developers allowed to contribute to your project?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Provide instructions on how to test your project.',
+        validate: testingInput => {
+            if (testingInput) {
+                return true;
+            } else {
+                console.log('You are required to provide testing instructions.');
+                return false;
+            }
+        }
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./genRead/generated-README.md', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'File has been created.'
+            });
+        });
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
-
+const init = () => {
+    return inquirer.prompt(questions)
+    .then(readmeData => {
+        return readmeData;
+    })
+}
 // Function call to initialize app
-init();
+init()
+.then(readmeData => {
+    console.log(readmeData);
+    return generateMarkdown(readmeData);
+})
+.then(pageMD => {
+    return writeFile(pageMD);
+})
+.then(writeFileResponse => {
+    console.log(writeFileResponse.message);
+})
+.catch(err => {
+    console.log(err);
+})
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,87 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'no license') {
+    return `[${license}](https://choosealicense.com/licenses/${license})`;
+  } else {
+    return ' ';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'no license') {
+    return `
+[${license}](https://choosealicense.com/licenses/${license})
+      `;
+    } else {
+      return ' ';
+    }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'no license') {
+    return `## [License](#table-of-contents) This project is covered under the following license: ${renderLicenseLink(license)}`;
+    } else {
+      return ' ';
+    }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+  ## Table-of-Contents
+  * [Description](#description)
+
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [Contributing](#contributing)
+
+  * [Tests](#tests)
+  
+  ## [Description]
+
+  ${data.description}
+
+  ## [Purpose]
+
+  ${data.why}
+
+  ## [What I Learned]
+
+  ${data.learn}
+
+
+  ## [Installation]
+
+  ${data.installation}
+
+
+  ## [Usage]
+
+  ${data.usage}
+  
+  
+  ${renderLicenseSection(data.license)}
+
+  ## [Contributing](#table-of-contents)
+  
+  
+  ## [Tests](#table-of-contents)
+
+  ${data.test}
+
+
+  ## [Questions](#table-of-contents)
+
+  Contact me:
+  [GitHub](https://github.com/${data.toastermaster27})
 
 `;
 }
